3945eb91256d7ee31914d6312b2fda62b4f090f7
Mert Ali <atemitdev@proton.me>
2022-12-01 14:41:33 +0300

    qol and part-2

--- day1/day1.py
+++ day1/day1.py
@@ -1,6 +1,12 @@
-file = open("day1.txt", "r")
+import os
+
+BASE = os.path.dirname(os.path.abspath(__file__))
+path = os.path.join(BASE, "day1.txt")
+file = open(path, "r")
 c=[]
 n=0
+d=[]
+part=input("Do you want the second part (N/y) ").upper()
 for i in file.readlines():
     if i != "\n":
         n+=int(i)
@@ -9,4 +15,12 @@ for i in file.readlines():
         n=0
 
 max_=max(c)
-print(max_)
+match part:
+    case "Y":
+        for i in range(3):
+            max_=max(c)
+            d.append(max_)
+            c.remove(max_)
+        print(sum(d))
+    case default:
+        print(max_)
